38790d7ee4403a365ad66da3cf8e294da34099f6
Tomasz Płuciennik <tomek_pl2@o2.pl>
2015-06-15 14:19:39 +0200

    Kolejna zmiana dotycząca czegośtam.

--- charset.php
+++ charset.php
@@ -1,5 +1,7 @@
 <?php
-echo 'To jest projekt z polskimi literami. Za¿ó³æ gê¶l± ja¼ñ. AAA BBB';
+echo 'To jest projekt z polskimi literami. Za¿ó³æ gê¶l± ja¼ñ. AAA BBB CCC';
 echo '<br />';
+echo 'Kolejne zdanie.';
+echo '<br/>';
 echo 'Dodanie kolejnego zdania';
 ?>
