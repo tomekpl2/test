8fa9bec3f6c2627acf885b494b4decd82453d608
Tomasz Płuciennik <tomek_pl2@o2.pl>
2015-06-15 14:56:39 +0200

    Komunikat

--- charset.php
+++ charset.php
@@ -3,7 +3,7 @@ echo 'To jest projekt z polskimi literami. Za¿ó³æ gê¶l± ja¼ñ. AAA BBB CCC DDD';
 echo '<br />';
 echo 'Kolejne zdanie.';
 echo '<br/>';
-echo 'Dodanie kolejnego zdania';
+echo 'Dodanie kolejnego zdania. skjdlkdsfjsfdlk';
 echo '<br />';
 echo 'Jeszcze jedno zdanie';
 ?>
