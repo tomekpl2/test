8d8abcc6633a20f85673841ad9678e572e1167f9
Piotr Gębura <p.gebura@wtk.pl>
2015-06-15 14:20:54 +0200

    Druga zmiana

--- charset.php
+++ charset.php
@@ -1,5 +1,7 @@
 <?php
-echo 'To jest projekt z polskimi literami. Za¿ó³æ gê¶l± ja¼ñ. AAA BBB';
+echo 'To jest projekt z polskimi literami. Za¿ó³æ gê¶l± ja¼ñ. AAA BBB DDD';
 echo '<br />';
 echo 'Dodanie kolejnego zdania';
+echo '<br />';
+echo 'Jeszcze jedno zdanie';
 ?>
