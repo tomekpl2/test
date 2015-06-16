96a2a30d38677e573ea82a56baeae5e5ab00b1fb
Piotr Gębura <p.gebura@wtk.pl>
2015-06-16 12:11:43 +0200

    add charstet to index

---
+++ charset.php
@@ -0,0 +1,13 @@
+<?php
+echo 'To jest projekt z polskimi literami. Za¿ó³æ gê¶l± ja¼ñ. AAA BBB CCC DDD EEE FFF';
+echo '<br />';
+echo 'Kolejne zdanie.';
+echo '<br/>';
+echo 'Dodanie kolejnego zdania. skjdlkdsfjsfdlk';
+echo '<br />Jaka¶ zmiana <br/>';
+echo 'Jeszcze jedno zdanie';
+echo '<br />';
+echo 'Zmiana w ostatnim zdaniu raz jeszcze';
+echo '<br />';
+echo 'Ostatnia linijka';
+?>
